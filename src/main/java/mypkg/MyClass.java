43b96b381aa9ea35c1f996ab6367e817290a3eb7
Tamilarasu <tamilarasup17@gmail.com>
2023-11-01 11:39:29 +0530

    First commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,27 @@
+package mypkg;
+import java.util.Scanner;
+public class MyClass {
+	public int areaReactangle(int l, int b) {
+		return(l*b);
+	}
+	public int square(int s) {
+		return(s*s);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass ob=new MyClass();
+		int l;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Length :");
+		l=sc.nextInt();
+		System.out.println("Enter breath :");
+		b=sc.nextInt();
+		System.out.println("Area of Reactangle is :"+ ob.areaReactangle(l, b));
+		
+		System.out.println("Area of Square is :"+ ob.square(10));		
+
+	}
+
+}
